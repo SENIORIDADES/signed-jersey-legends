38ec0482d00bdf821d1ab60c46a9a3aa4717ec02
devPisa <cesar.pisa@alunos.bpkedu.com.br>
2025-05-23 16:16:49 -0300

    Fix: route url deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/signed-jersey-legends',
   server: {
     host: "::",
     port: 8080,
